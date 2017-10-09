42cad98d0ea785824e30592f2f3a6ef4f3e91712
Sofyan Hadi Ahmad <sofyan@excellerate.co.id>
2017-10-09 16:50:35 +0700

    set api versioning

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,9 @@
+const apiVersion = process.env.EXCELLERIFY_API_VERSION
+
 var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://localhost:3000/api/v1/"'
+  API_URL: `"http://localhost:3000/api/${apiVersion + '/' || ''}"`
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,8 @@
 const apiHost = process.env.EXCELLERIFY_CUSTOM_HOST
 const apiPort = process.env.EXCELLERIFY_CUSTOM_PORT
+const apiVersion = process.env.EXCELLERIFY_API_VERSION
 
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: `"http://${apiHost}:${apiPort}/api/v1/"`
+  API_URL: `"http://${apiHost}:${apiPort}/api/${apiVersion + '/' || ''}"`
 }
