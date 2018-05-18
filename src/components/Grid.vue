9870d5a685a485e6c3f971876f55be96ca68aa40
Sofyan Hadi Ahmad <sofyan.ahmad@mandalalabs.com>
2018-05-18 13:59:28 +0700

    implement columns

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -132,6 +132,12 @@ v-flex(xs12)
                   v-btn(icon, @click="customAction(options.custom, props.item)")
                     v-icon {{options.custom.icon}}
 
+              v-list(v-if="options.customs")
+                v-list-tile(v-for="custom in options.customs")
+                  v-list-tile-content
+                    v-btn(icon, @click="customAction(custom, props.item)")
+                      v-icon {{custom.icon}}
+
     template(slot="no-data")
       tr
         td(:colspan="2 + columns.length", align="center")
@@ -139,11 +145,11 @@ v-flex(xs12)
 </template>
 
 <script>
-import moment from 'moment';
-import numeral from 'numeral';
+import moment from "moment";
+import numeral from "numeral";
 
-import EventBus from '../eventBus.js';
-import config from '../config';
+import EventBus from "../eventBus.js";
+import config from "../config";
 
 const getDefaultData = () => {
   return {
@@ -156,7 +162,7 @@ const getDefaultData = () => {
     columns: [], // fetch grid setup params from server if `columns` is empty
     actions: {},
     options: {
-      sort: 'id',
+      sort: "id",
       create: false,
       update: true,
       delete: false
@@ -164,7 +170,7 @@ const getDefaultData = () => {
     pagination: {
       page: 1,
       rowsPerPage: 10,
-      sortBy: 'id',
+      sortBy: "id",
       descending: true,
       totalItems: 0
     },
@@ -211,19 +217,19 @@ export default {
   data: getDefaultData,
 
   watch: {
-    '$i18n.locale'(val) {
+    "$i18n.locale"(val) {
       this.fetchGrid();
     },
-    'pagination.page'(val) {
+    "pagination.page"(val) {
       this.fetchData();
     },
-    'pagination.sortBy'(val) {
+    "pagination.sortBy"(val) {
       this.fetchData();
     },
-    'pagination.descending'(val) {
+    "pagination.descending"(val) {
       this.fetchData();
     },
-    '$route.params': 'refresh',
+    "$route.params": "refresh",
     pagination: {
       handler(val) {
         if (this.columns.length > 0) {
@@ -270,11 +276,11 @@ export default {
               return;
             }
 
-            if (key.indexOf('.') > -1) {
+            if (key.indexOf(".") > -1) {
               let nestedFilter = {
                 regexp: `/${val}/i`
               };
-              const splitedKey = key.split('.').reverse();
+              const splitedKey = key.split(".").reverse();
 
               this._.map(splitedKey, val => {
                 const prevNestedFilter = Object.assign(nestedFilter);
@@ -286,7 +292,7 @@ export default {
             } else {
               const column = this._.find(this.columns, { value: key });
 
-              if (column && column.type === 'date') {
+              if (column && column.type === "date") {
                 filters[key] = val;
               } else {
                 filters[key] = {
@@ -302,7 +308,7 @@ export default {
       const { sortBy, descending, page, rowsPerPage } = this.pagination;
 
       this.$route.query.filter = {
-        order: sortBy ? [`${sortBy} ${descending ? 'DESC' : 'ASC'}`] : null,
+        order: sortBy ? [`${sortBy} ${descending ? "DESC" : "ASC"}`] : null,
         where: filters,
         limit: rowsPerPage > 0 ? rowsPerPage : 0,
         offset: (page - 1) * rowsPerPage
@@ -328,23 +334,23 @@ export default {
     getColumnData(row, field) {
       let value = row[field.value];
 
-      if (field.type === 'image') {
-        const image = value ? global.config.api + value : 'static/noimage.png';
+      if (field.type === "image") {
+        const image = value ? global.config.api + value : "static/noimage.png";
 
         value = `<div class="avatar grey lighten-4" style="height: 36px; width: 36px;">
           <img src="${image}" alt="avatar">
         </div>`;
-      } else if (field.type === 'date') {
-        value = value ? moment(String(value)).format('YYYY-MM-DD') : '';
-      } else if (field.type === 'time') {
-        value = value ? moment(String(value)).format('HH:mm') : '';
-      } else if (field.type === 'datetime') {
-        value = value ? moment(String(value)).format('YYYY-MM-DD HH:mm') : '';
-      } else if (field.type === 'money') {
-        value = value ? numeral(value).format('0,0') : 0;
+      } else if (field.type === "date") {
+        value = value ? moment(String(value)).format("YYYY-MM-DD") : "";
+      } else if (field.type === "time") {
+        value = value ? moment(String(value)).format("HH:mm") : "";
+      } else if (field.type === "datetime") {
+        value = value ? moment(String(value)).format("YYYY-MM-DD HH:mm") : "";
+      } else if (field.type === "money") {
+        value = value ? numeral(value).format("0,0") : 0;
       } else {
         // process fields like `type.name`
-        const split = field.value.split('.');
+        const split = field.value.split(".");
 
         if (split.length > 0) {
           value = this._.get(row, field.value);
@@ -370,12 +376,12 @@ export default {
       try {
         this.loading = true;
 
-        const data = await this.$store.dispatch('fetchGridSchema', {
+        const data = await this.$store.dispatch("fetchGridSchema", {
           resource: this.resource,
           filter: this.filters
         });
 
-        if (!Array.isArray(data.columns) && typeof data.columns === 'object') {
+        if (!Array.isArray(data.columns) && typeof data.columns === "object") {
           data.columns = this.convertColumnObjToArray(data.columns);
         }
 
@@ -396,7 +402,7 @@ export default {
         this.options = data.options || {};
 
         if (this.options && this.options.sort) {
-          let sortData = this.options.sort.split('-');
+          let sortData = this.options.sort.split("-");
           let desc = sortData.length > 1;
           let sortField = sortData.pop();
 
@@ -424,7 +430,9 @@ export default {
           this.pagination.totalItems = parseInt(result.data.totalCount);
         } else {
           this.items = result.data;
-          this.pagination.totalItems = parseInt(result.headers['x-total-count']);
+          this.pagination.totalItems = parseInt(
+            result.headers["x-total-count"]
+          );
         }
 
         return {
@@ -453,7 +461,9 @@ export default {
           this.pagination.totalItems = parseInt(result.data.totalCount);
         } else {
           this.items = result.data;
-          this.pagination.totalItems = parseInt(result.headers['x-total-count']);
+          this.pagination.totalItems = parseInt(
+            result.headers["x-total-count"]
+          );
         }
 
         return {
@@ -468,7 +478,9 @@ export default {
     onDeleteBulk: async function() {
       if (this.selected.length > 0) {
         const itemIds = this.selected.map(item => item.id);
-        await this.$http.delete(`${this.resource}/multi/${JSON.stringify(itemIds)}`);
+        await this.$http.delete(
+          `${this.resource}/multi/${JSON.stringify(itemIds)}`
+        );
         this.refresh();
       }
     },
@@ -476,19 +488,27 @@ export default {
       this.pagination.page++;
     },
     customAction: async function(option, item) {
-      const { resource } = this;
+      let name = "customAction";
+      let path, resource, subResource;
 
-      let subResource,
-        name = 'customAction';
-
-      if (option.type === 'form') {
-        subResource = option.formUrl;
+      if (option.type === "form") {
+        path = option.formUrl;
         name = `${name}Form`;
-      } else if (option.type === 'grid') {
-        subResource = option.gridUrl;
+      } else if (option.type === "grid") {
+        path = option.formUrl;
         name = `${name}Grid`;
       } else {
-        throw new Error('Invalid action type');
+        throw new Error("Invalid action type");
+      }
+
+      const split = path.split("/");
+
+      if (path.split("/").length > 1) {
+        resource = split[0];
+        subResource = split[1];
+      } else {
+        resource = this.resource;
+        subResource = path;
       }
 
       this.$router.push({
@@ -500,22 +520,24 @@ export default {
           id: item.id
         },
         query: {
-          action: option.action || subResource,
-          method: option.method || 'POST'
+          action: option.action,
+          method: option.method || "POST"
         }
       });
     }
   },
   computed: {
     totalPages() {
-      return Math.ceil(this.pagination.totalItems / this.pagination.rowsPerPage);
+      return Math.ceil(
+        this.pagination.totalItems / this.pagination.rowsPerPage
+      );
     }
   },
   async mounted() {
     await this.refresh();
   },
   created() {
-    EventBus.$on('gridRefresh', this.refresh);
+    EventBus.$on("gridRefresh", this.refresh);
   }
 };
 </script>
